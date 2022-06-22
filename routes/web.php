022cd8b355ac3eb802157c1261c216cc2a778c01
Alperen Öztaş <alprnoztas@gmail.com>
2022-06-22 11:37:34 +0300

    Default route view  has changed to a employee page.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use App\Http\Controllers\EmployeeController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('employee.index');
 });
 
 Route::get('employees', [EmployeeController::class, 'index']);
