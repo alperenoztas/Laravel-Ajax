d73891fc539fd221b5266841845c65adc2ae2b7e
Alperen Öztaş <alprnoztas@gmail.com>
2022-06-28 16:29:05 +0300

    Types are changed

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -32,8 +32,8 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary add_employee">Save</button>
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-success add_employee">Save</button>
             </div>
 
         </div>
